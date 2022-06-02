5972d429542557187c5917b977c13427b6fb3679
Ray <42248123+Rayichi@users.noreply.github.com>
2022-06-03 00:36:40 +0200

    Update monitoring.sh

--- monitoring.sh
+++ monitoring.sh
@@ -4,10 +4,10 @@ arc=$(uname -srvmo)
 nproc=$(uname --all)
 vproc=$(cat /proc/cpuinfo | grep processor | wc -l)
 nram=$(free -m | awk 'NR==2{printf "%s/%s (%.2f%%)",$3,$2,$3*100/$2 }')
-ndisk=$(df -Bm -a |grep dev/mapper | awk '{sum+=$3}END{printf sum}')
-ndisk2=$(df -Bg -a |grep dev/mapper | awk '{sum+=$2}END{printf sum}')
+ndisk=$(df -Bm -a |grep dev/mapper | awk '{sum+=$3}END{printf sum "MB"}')
+ndisk2=$(df -Bg -a |grep dev/mapper | awk '{sum+=$2}END{printf sum "Gb"}')
 ndisk3=$(df -BM -a | grep /dev/mapper/ | awk '{sum1+=$3 ; sum2+=$2}END{printf "(%d%%)", (sum1)*100/sum2}')
-ncpu=$(top -bn1 | grep load | awk '{printf "#CPU Load: %.2f\n", $(NF-2)}')
+ncpu=$(top -bn1 | grep load | awk '{printf "%.2f%%\n", $(NF-2)}')
 nboot=$(who -b | awk '{printf $3 " " $4 " " $5}')
 nlvm=$(lsblk |grep lvm | awk '{if ($1) {print "yes";exit;} else {print "no"}}')
 ncon=$(netstat -an | grep ESTABLISHED | wc -l )
@@ -21,7 +21,7 @@ wall "
 	#CPU physical : $nproc
 	#vCPU : $vproc
 	#Memory Usage: $nram
-	#Disk Usage: $ndisk/$ndisk2Gb $ndisk3
+	#Disk Usage: $ndisk/$ndisk2 $ndisk3
 	#CPU load: $ncpu
 	#Last boot: $nboot
 	#LVM use: $nlvm
