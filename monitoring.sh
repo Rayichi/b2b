c7a42a47515fefeae43a6b7dfee435bd3f6dbed7
Ray <42248123+Rayichi@users.noreply.github.com>
2022-06-03 00:32:40 +0200

    Update monitoring.sh

--- monitoring.sh
+++ monitoring.sh
@@ -4,7 +4,7 @@ arc=$(uname -srvmo)
 nproc=$(uname --all)
 vproc=$(cat /proc/cpuinfo | grep processor | wc -l)
 nram=$(free -m | awk 'NR==2{printf "%s/%s (%.2f%%)",$3,$2,$3*100/$2 }')
-ndisk=$(df -Bg -a |grep dev/mapper | awk '{sum+=$3}END{printf sum}')
+ndisk=$(df -Bm -a |grep dev/mapper | awk '{sum+=$3}END{printf sum}')
 ndisk2=$(df -Bg -a |grep dev/mapper | awk '{sum+=$2}END{printf sum}')
 ndisk3=$(df -BM -a | grep /dev/mapper/ | awk '{sum1+=$3 ; sum2+=$2}END{printf "(%d%%)", (sum1)*100/sum2}')
 ncpu=$(top -bn1 | grep load | awk '{printf "#CPU Load: %.2f\n", $(NF-2)}')
@@ -13,7 +13,7 @@ nlvm=$(lsblk |grep lvm | awk '{if ($1) {print "yes";exit;} else {print "no"}}')
 ncon=$(netstat -an | grep ESTABLISHED | wc -l )
 nuse=$(who | wc -l)
 nip=$(hostname -I)
-nmac=$(ip a | grep link/ether | awk ' {print "(" $2} ")"')
+nmac=$(ip link show | grep link/ether | awk ' {print "(" $2 ")"}')
 nsudo=$(journalctl _COMM=sudo -q | wc -l)
 
 wall "
@@ -21,11 +21,11 @@ wall "
 	#CPU physical : $nproc
 	#vCPU : $vproc
 	#Memory Usage: $nram
-	#Disk Usage: $ndisk/$ndisk2 Mb $ndisk3
+	#Disk Usage: $ndisk/$ndisk2Gb $ndisk3
 	#CPU load: $ncpu
 	#Last boot: $nboot
 	#LVM use: $nlvm
-	#Connexions TCP : $ncon
+	#Connexions TCP : $ncon ESTABLISHED
 	#User log: $nuse
 	#Network: IP $nip $nmac
 	#Sudo : $nsudo cmd
